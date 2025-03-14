6b8f4e7a80a3b1d1354ec740ad24382ff32f179b
Gbenga Opeyemi <opesam42@gmail.com>
2025-03-14 03:57:03 +0100

    update wsgi.py:

--- eaglespoint/asgi.py
+++ eaglespoint/asgi.py
@@ -13,5 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eaglespoint.settings')
 
-# application = get_asgi_application()
-app = get_asgi_application() 
+application = get_asgi_application()
