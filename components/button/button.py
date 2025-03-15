2f9ea2cf5ed97418b5aa6929ca06b69061b9e5b2
Gbenga Opeyemi <opesam42@gmail.com>
2025-03-15 12:33:09 +0100

    update homepage

--- components/button/button.py
+++ components/button/button.py
@@ -4,8 +4,9 @@ from django_components import Component, register
 class Buttons(Component):
     template_file = "button/template.html"
 
-    def get_context_data(self, label="Click Me", href="#", type="primary"):
+    def get_context_data(self, tag="a", label="Click Me", href="#", type="primary"):
         return {
+            'tag': tag,
             'label': label,
             'href': href,
             'type': type,
--- manage.py
+++ manage.py
@@ -20,5 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# npx @tailwindcss/cli -i ./static/css/input.css -o ./static/css/output.css --watch
