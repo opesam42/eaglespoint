9dab8083e966b7f3a894afa01a48950d32963adc
Gbenga Opeyemi <opesam42@gmail.com>
2025-03-16 02:49:12 +0100

    Minor updates

--- components/button/button.py
+++ components/button/button.py
@@ -4,10 +4,11 @@ from django_components import Component, register
 class Buttons(Component):
     template_file = "button/template.html"
 
-    def get_context_data(self, html_tag="a", label="Click Me", href="#", type="primary"):
+    def get_context_data(self, html_tag="a", label="Click Me", href="#", type="primary", extraclass=None):
         return {
             'tag': html_tag,
             'label': label,
             'href': href,
             'type': type,
+            'extraclass': extraclass
         }
