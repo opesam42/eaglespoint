aab1f40abfa67238156c06892c69494f385b15d5
Gbenga Opeyemi <opesam42@gmail.com>
2025-04-06 09:37:38 +0100

    add filters modal component

---
+++ components/modal/modal.py
@@ -0,0 +1,11 @@
+from django_components import Component, register
+
+@register("modal")
+class Modal(Component):
+    template_file = "modal/template.html"
+
+    def get_context_data(self, modal_type, modal_title):
+        return {
+            'modal_type': modal_type,
+            'modal_title': modal_title,
+        }
