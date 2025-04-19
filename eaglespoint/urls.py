509c38bd04bf4d31b10ce73a48f91c34a06534f8
Gbenga Opeyemi <opesam42@gmail.com>
2025-04-19 14:54:48 +0100

    added append slash-true

--- eaglespoint/urls.py
+++ eaglespoint/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
     path("components/", include("django_components.urls")),
 ]
 
-if settings.DEBUG:  # Serve media files during development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:  # Serve media files during development
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
