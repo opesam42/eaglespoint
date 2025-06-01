7576f32c69b84e782b1904990fd842913f9af091
Gbenga Opeyemi <opesam42@gmail.com>
2025-06-01 16:03:45 +0100

    able to inject script from partial to dom

--- static/js/helper_func.js
+++ static/js/helper_func.js
@@ -30,6 +30,35 @@ function getFileName(fullUrl){
 }
 
 
+function loadModalForm(url, modalId, onOpenCallback=null){    
+    openLoadingModal()
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        }
+    })
+    .then(response => response.text())
+    .then(data => {
+        // since the data is a full html stuff, we need to extract the innerHTML of the body
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(data, 'text/html');
+        let html = doc.body.innerHTML;
+
+        let modal = document.getElementById(modalId)
+        injectHTMLWithScripts(modal.querySelector('.modal-body'), html);
+        
+        closeLoadingModal()
+        
+        if (typeof onOpenCallback === 'function') {
+            onOpenCallback()
+        }
+    })
+    .catch(error => console.log("Error:", error))
+}
+
+
 /**
  * Injects HTML into a container and re-executes any scripts inside the HTML.
  * @param {HTMLElement} container - The DOM element to inject HTML into.
@@ -41,17 +70,17 @@ function injectHTMLWithScripts(container, html) {
     const scripts = container.querySelectorAll('script');
 
     scripts.forEach(oldScript => {
-    const newScript = document.createElement('script');
+        const newScript = document.createElement('script');
 
-    // Copy all attributes from the original script tag
-    Array.from(oldScript.attributes).forEach(attr =>
-        newScript.setAttribute(attr.name, attr.value)
-    );
+        // Copy all attributes from the original script tag
+        Array.from(oldScript.attributes).forEach(attr =>
+            newScript.setAttribute(attr.name, attr.value)
+        );
 
-    // Copy inline script content
-    newScript.text = oldScript.textContent;
+        // Copy inline script content
+        newScript.text = oldScript.textContent;
 
-    // Replace old script with new one to execute it
-    oldScript.parentNode.replaceChild(newScript, oldScript);
+        // Replace old script with new one to execute it
+        oldScript.parentNode.replaceChild(newScript, oldScript);
     });
 }
