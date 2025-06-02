8955c8e6b00cf29897bbf8833708d520cae8628d
Gbenga Opeyemi <opesam42@gmail.com>
2025-06-02 13:29:01 +0100

    add blog to homepage

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from listing.models import Listings
 from utils.choices import LISTING_TYPE
+from blog.models import BlogArticle
 from cmscontent.models import Testimonial, FAQ, TeamMember
 
 # Create your views here.
@@ -8,7 +9,13 @@ from cmscontent.models import Testimonial, FAQ, TeamMember
 # travel page
 def home(request):
     testimonials = Testimonial.objects.filter(category='travel').order_by('?')
-    return render(request, 'core/index.html', {'testimonials': testimonials})
+    articles = BlogArticle.objects.filter(published=True).order_by('-created_at')[:3]
+
+    context = {
+        'testimonials': testimonials,
+        'articles': articles,
+    }
+    return render(request, 'core/index.html', context)
 
 
 def page_not_found_view(request, exception):
@@ -27,6 +34,8 @@ def real_estate_page(request):
     land_listings = all_listings.filter(listing_type='land')[:6]
     houses_for_sale = all_listings.filter(listing_type='buy')[:6]
     houses_for_rent = all_listings.filter(listing_type='rent')[:6]
+    articles = BlogArticle.objects.filter(published=True).order_by('-created_at')[:3]
+    testimonials = Testimonial.objects.filter(category='real_estate').order_by('?')
 
     if request.user.is_authenticated:
         user_favourites = Listings.objects.filter(is_listed=True, favourites__user=request.user)
@@ -40,6 +49,8 @@ def real_estate_page(request):
         'land_listings': land_listings,
         'houses_for_sale': houses_for_sale,
         'houses_for_rent': houses_for_rent,
+        'articles': articles,
+        'testimonials': testimonials,
     }
 
     return render(request, 'core/real_estate.html', context)
